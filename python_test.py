c54b1628ec0b8c14a571c2912c8c2e65cbeb9f58
juafeis <jmfelix@hotmail.com>
2020-11-13 14:27:24 +0100

    mergin into main

--- python_test.py
+++ python_test.py
@@ -6,14 +6,11 @@ import ordered_neopixel as neopixel
 def main():
     pixel_pin = board.D18
     pixels = neopixel.OrderedNeoPixel(pixel_pin, 60)
-    pixels[1] = (255, 0, 0)
+    pixels[5] = (255, 0, 0)
 
 
     pixels.update_color((0, 255, 0))
     #pixels.fill((0,255,0))
-
-
-
 
 
 if __name__ == '__main__':
