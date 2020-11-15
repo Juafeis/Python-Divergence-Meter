98c3f1369ce2b576482dbe30b6a815b4c9b1a3ed
juafeis <jmfelix@hotmail.com>
2020-11-15 07:23:57 +0100

    - added clcok functions to the ordered_neopixel class - added first implementation of clock to python_test

--- ordered_neopixel.py
+++ ordered_neopixel.py
@@ -2,6 +2,8 @@ import neopixel
 
 class OrderedNeoPixel(neopixel.NeoPixel):
 
+
+
     def __setitem__(self, key, value):
         i_pixel = self.mapping(key)
         super().__setitem__(i_pixel, value)
@@ -14,6 +16,19 @@ class OrderedNeoPixel(neopixel.NeoPixel):
         for i, pixel in enumerate(self):
             if pixel != [0, 0, 0]:
                 self[i] = color
+
+    def update_time(self, time, color):
+        self.fill((0,0,0))
+        if len(time) != 6:
+            raise Exception("Time must be composed by 6 numbers")
+        
+        i = 0
+        for number in time:
+            self[int(number) + i] = color
+            i += 10
+
+
+
 
     @staticmethod
     def mapping(n):
--- python_test.py
+++ python_test.py
@@ -1,16 +1,21 @@
 import board
 import time
+import datetime
 import ordered_neopixel as neopixel
 
 
 def main():
     pixel_pin = board.D18
+    nixie_color = (255, 20, 0)
     pixels = neopixel.OrderedNeoPixel(pixel_pin, 60)
-    pixels[5] = (255, 0, 0)
+    #pixels[3] = nixie_color
+    while(True):
+        _time = datetime.datetime.now().strftime("%H%M%S")
+        pixels.update_time(_time, nixie_color)
+        time.sleep(1)
 
 
-    pixels.update_color((0, 255, 0))
-    #pixels.fill((0,255,0))
+    #pixels.update_color(nixie_color)
 
 
 if __name__ == '__main__':
